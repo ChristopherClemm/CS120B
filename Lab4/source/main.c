1610aac11a9dd0fe44f871ca937f78a1aabf57e5
Christopher Clemm <cclem004@wch136-30.cs.ucr.edu>
2020-04-16 20:28:23 -0700

    added lab4

---
+++ Lab4/source/main.c
@@ -0,0 +1,73 @@
+/*	Author: Christopher Clemm4
+ *  Partner(s) Name: NA
+ *	Lab Section:23
+ *	Assignment: Lab #4  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+	DDRA = 0x00; PORTA = 0xFF; //input
+	DDRB = 0xFF; PORTB = 0x00; //output
+	//DDRC = 0xFF; PORTC = 0x00; //output
+	
+	enum States{Init, LED1ON, LED2ON)state;
+
+	unisgned char button, LEDB0, LEDB1;
+	button = 0x00;
+	LEDB0 = 0x00;
+	LEDB1 =0x00;
+
+
+int main(void) {
+	button = PINA;
+	
+	switch(state)
+	{
+
+		case Init: 
+			state = LED1ON;
+			break;
+		case LED1ON:
+			if((PINA & 0x01 ) == 0x01)
+			{
+				state = LED1ON;
+			}
+			else
+			{
+				state = LED2ON;
+			}
+			break;
+		case LED2ON:
+			if(PINA & 0x01) == 0x00)
+			{
+				state = LED2ON;
+			}
+			else
+			{
+				state = LED1ON;		
+			}
+			break;
+		case default:
+			state = INit;
+			break;
+	}
+
+	s		 
+
+
+	}
+	
+    /* Insert DDR and PORT initializations */
+	
+    /* Insert your solution below */
+    while (1) {
+
+    }
+    return 1;
+}
